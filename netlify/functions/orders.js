5ae4bd30035f8a52d9a2ea12e4f239b2ab43c8a5
zackleejohnston-ops <zackleejohnston@gmail.com>
2026-06-13 16:07:04 -0600

    Create orders.js

---
+++ netlify/functions/orders.js
@@ -0,0 +1,34 @@
+const https = require('https');
+exports.handler = async function(event, context) {
+  const API_KEY = '44820105A0C483295BC3DD05E404E55E72EA3A6FAA470C02A476DDCB3C2A2AE5';
+  return new Promise((resolve) => {
+    const options = {
+      hostname: 'ocelotlogistics.infopluswms.com',
+      port: 443,
+      path: '/infoplus-wms/api/v3/order/search?filter=orderNo%20gt%200&limit=50&sort=-orderNo',
+      method: 'GET',
+      headers: {
+        'API-Key': API_KEY,
+        'Accept': 'application/json'
+      }
+    };
+    const req = https.request(options, (res) => {
+      let data = '';
+      res.on('data', chunk => data += chunk);
+      res.on('end', () => {
+        resolve({
+          statusCode: 200,
+          headers: { 'Access-Control-Allow-Origin': '*' },
+          body: data
+        });
+      });
+    });
+    req.on('error', (err) => {
+      resolve({
+        statusCode: 500,
+        body: JSON.stringify({ error: err.message })
+      });
+    });
+    req.end();
+  });
+};
