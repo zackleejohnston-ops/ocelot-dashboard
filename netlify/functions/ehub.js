36b3173ea38141c3ded06510b1833193588c80f5
zackleejohnston-ops <zackleejohnston@gmail.com>
2026-06-13 16:45:01 -0600

    Update ehub.js

--- netlify/functions/ehub.js
+++ netlify/functions/ehub.js
@@ -3,9 +3,9 @@ exports.handler = async function(event, context) {
   const EH_KEY = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpYXQiOjE3ODExMDM3MjksImRhdGEiOnsidXNlciI6eyJpZCI6MjExMzEsImN1c3RvbWVyX2lkIjoxMDgxMSwiZW1haWwiOiJ6YWNrbGVlam9obnN0b25AZ21haWwuY29tIn0sInNjb3BlcyI6WyJhcGlfcHVibGljIl19fQ.t_axIrFMt0vSjiZ3sQuignuOkadEV2Ux5r2717C6gAKsbIR-e1Ak7RCnaTVbX1SLfSf3AKniSj7aSX7Gj24h9A';
   return new Promise((resolve) => {
     const options = {
-      hostname: 'admin.ehub.com',
+      hostname: 'app.ehub.com',
       port: 443,
-      path: '/api/v1/shipments?per_page=100',
+      path: '/api/v2/shipments?per_page=100',
       method: 'GET',
       headers: {
         'Authorization': 'Bearer ' + EH_KEY,
