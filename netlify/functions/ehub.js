1717154c2bb94902e374e3f97ef2a9eedd6a4054
zackleejohnston-ops <zackleejohnston@gmail.com>
2026-06-15 08:55:34 -0600

    Update ehub.js

--- netlify/functions/ehub.js
+++ netlify/functions/ehub.js
@@ -1,14 +1,16 @@
 const https = require('https');
 exports.handler = async function(event, context) {
   const EH_KEY = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpYXQiOjE3ODExMDM3MjksImRhdGEiOnsidXNlciI6eyJpZCI6MjExMzEsImN1c3RvbWVyX2lkIjoxMDgxMSwiZW1haWwiOiJ6YWNrbGVlam9obnN0b25AZ21haWwuY29tIn0sInNjb3BlcyI6WyJhcGlfcHVibGljIl19fQ.t_axIrFMt0vSjiZ3sQuignuOkadEV2Ux5r2717C6gAKsbIR-e1Ak7RCnaTVbX1SLfSf3AKniSj7aSX7Gj24h9A';
-
   return new Promise((resolve) => {
     const options = {
       hostname: 'app.ehub.com',
       port: 443,
-      path: '/api/v2/shipments?per_page=100&page=2800',
+      path: '/api/v2/shipments?per_page=100&page=100',
       method: 'GET',
-      headers: { 'Authorization': 'Bearer ' + EH_KEY, 'Accept': 'application/json' }
+      headers: {
+        'Authorization': 'Bearer ' + EH_KEY,
+        'Accept': 'application/json'
+      }
     };
     const req = https.request(options, (res) => {
       let data = '';
